af37710abe748e8cbf258d92a108aac0c2abd3f4
Anderson Ramirez <andersongamer037@gmail.com>
2023-04-06 22:52:06 +0000

    9. Fizz-Buzz

---
+++ 9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+ * program that prints the numbers from 1 to 100 with the "Fizz-Buzz test".
+ * 
+ * return 0.
+*/
+int main(){
+    int i;
+    for(i = 1; i <= 100; i++){
+        if(i % 3 == 0 && i % 5 == 0){
+            printf("FizzBuzz ");
+            continue;
+        } else if (i % 3 == 0 && i % 5 != 0){
+            printf("Fizz ");
+            continue;
+        } else if(i % 3 != 0 && i % 5 == 0){
+            printf("Buzz ");
+        } else{
+            printf("%d ",i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
