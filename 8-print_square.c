8d37f548f68cd4c1c84697beb2c7348a086f70e2
Anderson Ramirez <andersongamer037@gmail.com>
2023-04-06 22:29:12 +0000

    8. You are so much sunshine in every square inch

---
+++ 8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * print_square - function that prints a square.
+ * @int
+ * return nothing
+*/
+void print_square(int size){
+    int a, b;
+    if(size == 0){
+        _putchar('\n');
+    } else{
+        for(a = 1; a <= size; a++){
+            for(b = 1; b <= size; b++){
+            _putchar(35);
+            }
+            _putchar('\n');
+        }
+    }
+}
